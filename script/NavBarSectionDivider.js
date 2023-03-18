4bf7c8607490c3301350bd8afdfae7db474bf82f
Glen Andrew <90735671+dreeew05@users.noreply.github.com>
2023-03-18 23:58:06 +0800

    19/03/2023 commit

---
+++ script/NavBarSectionDivider.js
@@ -0,0 +1,21 @@
+// Author: fiVe
+// Description: Active Link when link is clicked
+
+export class NavBarSectionDivider {
+    constructor(links) {
+       this.links = links; 
+
+       this.changeActiveLink(links)
+    }
+    changeActiveLink(allLinks){
+        allLinks.forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                allLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                this.classList.add('active');
+            });
+        });
+    }
+}  
---
+++ script/ProfilePage.js
@@ -0,0 +1,7 @@
+//Author: fiVe
+//Description:
+
+import { NavBarSectionDivider } from "./NavBarSectionDivider.js";
+
+const links  = document.querySelectorAll('.nav-item a');
+const nbsd = new NavBarSectionDivider(links);
