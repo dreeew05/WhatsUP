4a2f90646ec7d56f6040ad87efae4d3fcde0b1e7
Glen Andrew <90735671+dreeew05@users.noreply.github.com>
2023-03-21 13:57:04 +0800

    Convert ProfilePage.js to OOP

--- script/ProfilePage.js
+++ script/ProfilePage.js
@@ -2,13 +2,22 @@
 //Description: JavaScript for profilePage
 
 import { NavBarSectionDivider } from "./classes/NavBarSectionDivider.js";
-
-const links  = document.querySelectorAll('.nav-item a');
-const nbsd   = new NavBarSectionDivider(links);
-
 import { GeneratePostImage } from "./classes/GeneratePostImage.js";
 
-let filenames = ["cityCampus", "cityCampus2", "cityCampus3", "cityCampus4"],
-    captions  = ["Team Picture", "Balay Ilonggo", "Front Gate UP - Visayas", "LGBT OBLE"];
+class ProfilePage {
+    constructor() {
+        this.navBarSectionDividerImplementation();
+        this.generatePostImage();
+    }
+    navBarSectionDividerImplementation() {
+        const links  = document.querySelectorAll('.nav-item a');
+        new NavBarSectionDivider(links);
+    }
+    generatePostImage() {
+        let filenames = ["cityCampus", "cityCampus2", "cityCampus3", "cityCampus4"],
+            captions  = ["Team Picture", "Balay Ilonggo", "Front Gate UP - Visayas", "LGBT OBLE"];
+        new GeneratePostImage(filenames.length, filenames, captions);
+    }
+}
 
-new GeneratePostImage(filenames.length, filenames, captions);
+new ProfilePage();
