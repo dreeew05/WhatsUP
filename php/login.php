3176c4cf0577b22aa0a61a893429ba9eaa08fbcb
daneeeeeee <100850374+daneeeeeee@users.noreply.github.com>
2023-06-04 03:11:54 +0800

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "whatsup";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['pass'];
+
+    $result = ("SELECT * FROM profile_credentials WHERE Username = '$username' AND Password = '$password'");
+    $log = $conn->query($result);
+
+    if ($log->num_rows > 0){
+        while ($row = $log->fetch_assoc()){
+            $_SESSION['username'] = $row['Username'];
+            $_SESSION['password'] = $row['Password'];
+            header('Location: profilePage.php'); 
+            exit();
+        }
+    }
+    else {
+        echo "Invalid Username or Password.";
+    }
+}
+
+?>
