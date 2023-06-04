f84425bf6586c7e8d7a67b3bfb25894cbd35e458
Glen Andrew <90735671+dreeew05@users.noreply.github.com>
2023-06-04 11:08:39 +0800

    Create IDGetter.php
    Gets the Id to generate modals

---
+++ php/IDGetter.php
@@ -0,0 +1,50 @@
+<?php
+   
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "whatsup";
+
+   // Create a connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+
+   // Check the connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+
+   // Request from the front-end
+   $request = json_decode(
+      file_get_contents('php://input'),
+      true
+   );
+
+   switch($request['type']) {
+      case 'post':
+         $SQL = "SELECT MAX(PostID) As MaxID
+                 FROM post";
+         break;
+      case 'thread':
+         $SQL = "SELECT MAX(ThreadID) As MaxID
+                 FROM thread";
+         break;
+      default:
+         break;
+   }
+
+   $RESULT = $conn -> query($SQL);
+
+   if($RESULT) {
+      $row   = $RESULT -> fetch_assoc();
+      $maxID = $row['MaxID'];
+   }
+
+   echo json_encode(
+      array(
+         'maxID' => $maxID
+      )
+   );
+
+   $conn -> close();
+      
+?>
