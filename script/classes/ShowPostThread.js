3f9b5f4a92a0e2d8baf4c6aa9fc345b0dac0aecd
Glen Andrew <90735671+dreeew05@users.noreply.github.com>
2023-06-05 18:26:14 +0800

    Update ShowPostThread.js
    Now working hehe
    
    Co-Authored-By: Briana Jade Adricula <88810879+Brianadrix@users.noreply.github.com>
    Co-Authored-By: daneeeeeee <100850374+daneeeeeee@users.noreply.github.com>

--- script/classes/ShowPostThread.js
+++ script/classes/ShowPostThread.js
@@ -3,91 +3,75 @@
 
 import { FeedGenerator } from "./FeedGenerator.js";
 import { NavBarFactory } from "./NavBarFactory.js";
-
-// TEST DATA
-import postsJSON from "../../test/posts.json" assert { type: 'json' };
-import threadJSON from "../../test/threads.json" assert { type: 'json' };
 import { GeneratePostMap } from "./GeneratePostMap.js";
+import { DataSerializer } from "./DataSerializer.js";
 
 class ShowPostThread {
     constructor() {
         // GLOBAL VARIABLE
-        this.mapAPI = new GeneratePostMap();
+        this.postData       = null;
+        this.threadData     = null;
+        this.mapAPI         = new GeneratePostMap();
+        this.dataSerializer = new DataSerializer();
 
-        // TEST DATA
-        this.postID      = null;
-        this.threadID    = null;
-        this.postObject  = null;
-        this.threadArray = [];
-
-        this.getData(); //TEST DATA
-
+        // METHODS
         this.initializeNavBar();
+        this.initializeContent();
+    }
+
+    getPostData() {
+        return this.postData;
+    }
+
+    getThreadData() {
+        return this.threadData;
+    }
+
+    setPostData(postData) {
+        this.postData = postData;
+    }
+
+    setThreadData(threadData) {
+        this.threadData = threadData;
+    }
+
+    async initializeContent() {
+        const url    = window.location.href,
+              urlObj = new URL(url),
+              type   = urlObj.searchParams.get('type'),
+              id     = urlObj.searchParams.get('id');
+
+        if(type && id) {
+            const request = {
+                'mode' : null,
+                'id'   : id
+            }
+            let postResponse,
+                threadResponse;
+            switch(type) {
+                case 'post':
+                    request.mode = 'showPost';
+                    postResponse = await this.dataSerializer.postData(
+                        request, '/php/PostGetter.php'
+                    );
+                    request.mode = 'showThread';
+                    threadResponse = await this.dataSerializer.postData(
+                        request, '/php/ThreadGetter.php'
+                    );
+                    break;
+                case 'thread':
+                    request.mode = 'showThread';
+                    break;
+                default:
+                    break;
+            }
+            this.setPostData(postResponse[0]);
+            this.setThreadData(threadResponse);
+        }
         this.initializeFeedGenerator();
         this.goBack();
-    }
-
-    // TEST DATA
-    // SAMPLE. NOT PART OF FINAL
-    // PASSED OBJECT MUST COME FROM SQL 
-    // VISUALIZATION PURPOSES THROUGH JSON
-    // [START]
-    getData() {
-        let locationHeader = window.location.href,
-            type           = null,
-            id             = null;
-
-        locationHeader = locationHeader.split("&");
-        type           = locationHeader[0].split("=")[1],
-        id             = locationHeader[1].split("=")[1];
-
-        if(type == "post") {
-            this.getPostData(id);
-        }
-        else {
-            this.getPostIDFromThreads(id);
-        }
-
-        // GO TO TARGET ID
-        this.scrollIntoID(type, id);
 
     }
-
-    getPostObject() {
-        return this.postObject;
-    }
-
-    setPostObject(obj) {
-        this.postObject = obj;
-    }
-
-    getThreadsArray() {
-        return this.threadArray;
-    }
-
-    getPostData(id) {
-        for(let i = 0; i <  postsJSON.length; i++){
-            if(postsJSON[i].id == id) {
-                this.setPostObject(postsJSON[i]);
-                break;
-            }
-        }
-        for(let i = 0; i < threadJSON.length; i++) {
-            if(threadJSON[i].post_id == id) {
-                this.getThreadsArray().push(threadJSON[i]);
-            }
-        }
-    }
-
-    getPostIDFromThreads(id) {
-        for(let i = 0; i < threadJSON.length; i++) {
-            if(threadJSON[i].id == id) {
-                this.getPostData(threadJSON[i].post_id);
-                break;
-            }
-        }
-    }
-    // [END]
 
     initializeNavBar() {
         new NavBarFactory("type1", "visitor");
@@ -105,14 +89,15 @@ class ShowPostThread {
     }
 
     initializeFeedGenerator() {
-        let feedGenerator = new FeedGenerator(this.mapAPI);
-        // console.log(this.getThreadsArray())
-        // console.log(this.getPostObject())
+        const feedGenerator = new FeedGenerator(this.mapAPI);
 
-        feedGenerator.initializeSideNavBar();
-        // TEST DATA
-        feedGenerator.generatePost(this.getPostObject());
-        feedGenerator.generateThread(this.getThreadsArray());
+        feedGenerator.generatePost(
+            this.getPostData()
+        );
+        feedGenerator.generateThread(
+            this.getThreadData()
+        );
+        
     }
 
     goBack() {
